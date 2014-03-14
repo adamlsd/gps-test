ed427d7c1d89c6953200f6b33ce956357654f59b
adamlsd <adamartin@freebsd.org>
2014-03-14 16:27:07 -0400

    A few simple tests to require expected methods
    Nothing too fancy, but these tests should check for the presence, 
    and simple functionality of the required gpstring APIs.

--- tests/simple.cc
+++ tests/simple.cc
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <exception>
 #include <cstdlib>
+#include <cassert>
+
+#include "gpstring.h"
+#include "linklist.h"
 
 int
 main()
 try
 {
+  gpstring s( "Hello World" );
+  const gpstring s2= s;
+  
+  s+= "Goodbye World";
+  
+  const gpstring gbw= "Goodbye World";
+  
+  assert( s != s2 );
+  
+  assert( "Oops" + s != s );
+  assert( s + "OOPS" != s );
+  
+  assert( s + gbw == s2 );
+  
+  assert( gbw + s != s2 );
+
+  assert( gbw[ 4 ] == 'b' );
+
   return EXIT_SUCCESS;
 }
 catch( const std::exception &ex )
